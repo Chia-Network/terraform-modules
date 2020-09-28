3dd7f0f1db69a85fc2777e741ea9e4677828ff10
justinengland <justin@jeng.land>
2020-09-28 17:16:19 -0600

    removing un needed comment

--- ssl/aws_iam_cert/aws_iam_cert.tf
+++ ssl/aws_iam_cert/aws_iam_cert.tf
@@ -8,10 +8,6 @@ resource "aws_iam_server_certificate" "certificate" {
   certificate_body  = var.ssl_cert
   private_key       = var.ssl_key
 
-  # Some properties of an IAM Server Certificates cannot be updated while they
-  # are in use. In order for Terraform to effectively manage a Certificate in
-  # this situation, it is recommended you utilize the name_prefix attribute and
-  # enable the create_before_destroy lifecycle block.
   lifecycle {
     create_before_destroy = true
   }
