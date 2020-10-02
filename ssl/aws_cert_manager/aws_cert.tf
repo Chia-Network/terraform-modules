a66aa60e961871c17350d14276407e7ffc5edf21
justinengland <justin@jeng.land>
2020-10-01 20:36:09 -0600

    removing bad characters

--- ssl/aws_cert_manager/aws_cert.tf
+++ ssl/aws_cert_manager/aws_cert.tf
@@ -11,9 +11,9 @@ resource "aws_acm_certificate" "cert" {
 module "dns" {
   source            = "git@github.com:Chia-Network/terraform-modules.git//cloudflare_dns"
   zone_id           = var.zone_id
-  record_name       = ${aws_acm_certificate.default.domain_validation_options.cert.resource_record_name}
-  record_value      = ${aws_acm_certificate.default.domain_validation_options.cert.resource_record_value}
-  record_type       = ${aws_acm_certificate.default.domain_validation_options.cert.resource_record_type}
+  record_name       = aws_acm_certificate.default.domain_validation_options.cert.resource_record_name
+  record_value      = aws_acm_certificate.default.domain_validation_options.cert.resource_record_value
+  record_type       = aws_acm_certificate.default.domain_validation_options.cert.resource_record_type
   providers         = {
       cloudflare = cloudflare
     }
