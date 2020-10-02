83b2f2d93c637680a8095ffe674d183f244d7c1f
justinengland <justin@jeng.land>
2020-10-01 18:01:55 -0600

    adding aws acm certs

---
+++ ssl/aws_cert_manager/aws_cert.tf
@@ -0,0 +1,20 @@
+#
+# AWS ACM SSL Certificate
+#  * cert, validation
+#
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain
+  validation_method = "DNS"
+}
+
+module "dns" {
+  source            = "git@github.com:Chia-Network/terraform-modules.git//cloudflare_dns"
+  zone_id           = var.zone_id
+  record_name       = ${aws_acm_certificate.default.domain_validation_options.0.resource_record_name}
+  record_value      = ${aws_acm_certificate.default.domain_validation_options.0.resource_record_value}
+  record_type       = ${aws_acm_certificate.default.domain_validation_options.0.resource_record_type}
+  providers         = {
+      cloudflare = cloudflare
+    }
+}
