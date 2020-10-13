645819624bb92a2e9ffa2bbb7652730d1d134f89
justinengland <justin@jeng.land>
2020-10-13 17:01:46 -0600

    adding base github runner module

---
+++ github-runner/github-runner.tf
@@ -0,0 +1,37 @@
+#
+# Ec2 Resources
+#  * GithubRunner
+#
+#
+
+resource "aws_instance" "ChiaRunner" {
+  count                = var.instance_count
+  instance_type        = var.instance_type
+  iam_instance_profile = var.iam_instance_profile
+  ami                  = var.ami
+  security_groups      = var.security_groups
+  subnet_id            = var.subnet_id
+
+  tags = {
+  Name = "${ var.runner_name }-${count.index + 1}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "/home/ubuntu/actions-runner/config.sh --url https://github.com/Chia-Network --token ${ var.runner_token } --labels Chia --runnergroup ChiaRunners --unattended --replace",
+      "cd /home/ubuntu/actions-runner && sudo ./svc.sh install",
+      "cd /home/ubuntu/actions-runner && sudo ./svc.sh start",
+      #"sudo su && hostnamectl set-hostname ${ var.runner_name }-${count.index + 1}",
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
+  lifecycle {
+  create_before_destroy = true
+  }
+}
---
+++ github-runner/variables.tf
@@ -0,0 +1,52 @@
+variable "instance_count" {
+default = "3"
+}
+
+variable "runner_name" {
+  description = "Github Runner Name"
+  type = string
+}
+
+variable "ec2_user" {
+  description = "instance ec2 user"
+  type        = string
+}
+
+variable "ec2_key" {
+  description = "instance private key file"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "instance size/type"
+  type = string
+  default = "t3.medium"
+}
+
+variable "ami" {
+  description = "instance ami id"
+  type = string
+}
+
+variable "iam_instance_profile" {
+  description = "IAM Instance Profile to use for Resource"
+  type = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the VPC that the instance security group belongs to"
+}
+
+variable "security_groups" {
+  description = "Security Group for resources"
+}
+
+variable "runner_token" {
+  description = "the github token for registering runners"
+}
+
+variable "tags" {
+  description = "Additional tags"
+  type        = map
+  default     = {}
+}
