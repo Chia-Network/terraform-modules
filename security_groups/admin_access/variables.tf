f555785e83badce409bb2a084771b883e5b631fb
justinengland <justin@jeng.land>
2020-10-20 15:20:07 -0600

    pairing down vars

--- security_groups/admin_access/variables.tf
+++ security_groups/admin_access/variables.tf
@@ -2,22 +2,8 @@ variable "custom_sg_rules" {
   description = "used to pass custom rules to the module"
 }
 
-variable "vpn_cidr_block" {
-  description = "cidr block of the vpn server"
-  type        = string
-}
-
-variable "vpn_ipv6_cidr_block" {
-  description = "ipv6 cidr block of the vpn server"
-  type        = string
-}
-
 variable "vpc" {
   description = "ID of main VPC in region"
-}
-
-variable "security_group_ids" {
-  description = "IDs of the security groups we are allowing access"
 }
 
 variable "application_tag" {
