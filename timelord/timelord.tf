89e46f5c93a649c18627683c2b874f433c44cc93
justinengland <justin@jeng.land>
2020-10-21 18:52:51 -0600

    spacing and other concerns on line 12

--- timelord/timelord.tf
+++ timelord/timelord.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "timelord" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.security_group_main},var.admin_sg]
+  vpc_security_group_ids = [ var.security_group_main,var.admin_sg ]
   key_name               = var.key_name
   #iam_instance_profile = var.instance-role+
 
