1c1dc097b21b9071d7fb2e6579f68cdd4ce8a515
justinengland <justin@jeng.land>
2020-11-25 03:26:40 -0700

    adding farmer module

---
+++ farmer/farmer.tf
@@ -0,0 +1,48 @@
+#
+# Ec2 Resources
+#  * timelord
+#
+
+
+resource "aws_instance" "farmer" {
+  count                  = var.instance_count
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [ var.security_group_main,var.admin_sg ]
+  key_name               = var.key_name
+  #iam_instance_profile = var.instance-role+
+
+  tags = {
+  Name = "TestNet-Farmer-${count.index + 1}"
+  application = var.application_tag
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export CHIA_ROOT=~/.chia",
+      "cd /home/ubuntu/chia-blockchain",
+      ". ./activate",
+      "sh ./install.sh",
+      "chia init",
+      "chia keys generate",
+      "chia init",
+      "chia start farmer",
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
+  #root_block_device {
+  #  volume_size = var.volume_size
+  #}
+
+  lifecycle {
+  create_before_destroy = true
+
+  }
+}
