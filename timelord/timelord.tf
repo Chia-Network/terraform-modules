975a9d0ada41b890b3b91ab9f03fb95fc887d426
justinengland <justin@jeng.land>
2020-11-30 13:27:17 -0700

    adding chia blockchain copy to timelord module

--- timelord/timelord.tf
+++ timelord/timelord.tf
@@ -18,6 +18,17 @@ resource "aws_instance" "timelord" {
   application = var.application_tag
   }
 
+  provisioner "file" {
+    source      = "./chia-blockchain"
+    destination = "/home/ubuntu/chia-blockchain"
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
   provisioner "remote-exec" {
     inline = [
       "export CHIA_ROOT=~/.chia",
