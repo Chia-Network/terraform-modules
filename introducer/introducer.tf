ba5d7d53c0a7671c4c75c778268780583a7fd484
niraj <npp5048@gmail.com>
2020-11-30 19:28:15 -0500

    added introducer module

---
+++ introducer/introducer.tf
@@ -0,0 +1,47 @@
+#
+# Ec2 Resources
+#  * introducer
+#
+
+
+resource "aws_instance" "introducer" {
+  count                  = var.instance_count
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [ var.security_group_main,var.admin_sg ]
+  key_name               = var.key_name
+  #iam_instance_profile = var.instance-role+
+
+  tags = {
+  Name = "ChiaIntroducer-${count.index + 1}"
+  application = var.application_tag
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export CHIA_ROOT=~/.chia",
+      "cd /home/ubuntu/chia-blockchain",
+      ". ./activate",
+      "chia init",
+      "chia keys generate",
+      "chia init",
+      "chia start introducer",
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
+  root_block_device {
+    volume_size = var.volume_size
+  }
+
+  lifecycle {
+  create_before_destroy = true
+
+  }
+}
