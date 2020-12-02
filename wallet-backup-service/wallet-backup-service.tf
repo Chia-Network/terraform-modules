6d70bd0032e090dba1e282af09bca4cda0ff334e
justinengland <justin@jeng.land>
2020-12-01 21:38:50 -0700

    changing backup module to match the rest.

--- wallet-backup-service/wallet-backup-service.tf
+++ wallet-backup-service/wallet-backup-service.tf
@@ -18,6 +18,56 @@ resource "aws_instance" "BackupNode" {
   application = var.application_tag
   }
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo rm -rf /home/ubuntu/chia-blockchain/ && sudo mkdir /home/ubuntu/chia-blockchain && sudo chown -R ubuntu:ubuntu /home/ubuntu/chia-blockchain",
+      "sudo rm -rf /home/ubuntu/wallet-backup-service/ && sudo mkdir /home/ubuntu/wallet-backup-service && sudo chown -R ubuntu:ubuntu /home/ubuntu/wallet-backup-service",
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
+  provisioner "file" {
+    source      = "./chia-blockchain"
+    destination = "/home/ubuntu/"
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
+  provisioner "file" {
+    source      = "./wallet-backup-service"
+    destination = "/home/ubuntu/"
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "ln -s /home/ubuntu/chia-blockchain /home/ubuntu/wallet-backup-service/chia-blockchain",
+      "export CHIA_ROOT=~/.chia",
+      "cd /home/ubuntu/wallet-backup-service && sudo chmod a+x ./install_ubuntu.sh && sh ./install_ubuntu.sh",
+      "systemctl restart backup-service",
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
   lifecycle {
     create_before_destroy = true
   }
