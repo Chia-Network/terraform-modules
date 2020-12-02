f6369b6124913ca6abf6deb11faf85b1d52c3c06
justinengland <justin@chia.net>
2020-12-02 00:44:02 -0700

    adding main node module

---
+++ main-node/variables.tf
@@ -0,0 +1,64 @@
+variable "instance_count" {
+default = "1"
+}
+
+variable "volume_size" {
+default = "10"
+}
+
+variable "ec2_user" {
+  description = "instance ec2 user"
+  type        = string
+}
+
+variable "ec2_key" {
+  description = "instance private key file"
+  type        = string
+}
+
+variable "key_name" {
+  description = "instance private key file aws name"
+  type        = string
+}
+
+variable "ami" {
+  description = "instance ami id"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "instance size/type"
+  type        = string
+  default     = "c5.9xlarge"
+}
+
+variable "subnet_id" {
+  description = "The ID of the VPC that the instance security group belongs to"
+  type        = string
+}
+
+variable "main_sg" {
+  description = "Main Service Security Group for resources"
+}
+
+variable "admin_sg" {
+  description = "admin sg"
+}
+
+variable "iam_instance_profile" {
+   description = "IAM Instance Profile to use for Resource"
+   type        = string
+   default     = ""
+}
+
+variable "application_tag" {
+  description = "the instance tag to use"
+  type        = string
+  default     = "testnet"
+}
+
+variable "tags" {
+  description = "Additional tags"
+  type        = map
+  default     = {}
+}
