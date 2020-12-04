c2036b42709afb3dd982930b351232122eb1a809
justinengland <justin@chia.net>
2020-12-03 21:54:36 -0700

    trying output changes

--- timelord/outputs.tf
+++ timelord/outputs.tf
@@ -1,14 +1,14 @@
 output "public_dns" {
-value       = aws_instance.timelord[0].public_dns
+value       = "${join(",", aws_instance.timelord.*.public_dns)}"
 description = "the public dns for this timelord instance"
 }
 
 output "public_ip" {
-value       = aws_instance.timelord[0].public_ip
+value       = "${join(",", aws_instance.timelord.*.public_ip)}"
 description = "the public ipv4 for this timelord instance"
 }
 
 output "ipv6_addresses" {
-value       = aws_instance.timelord[0].ipv6_addresses
+value       = "${join(",", aws_instance.timelord.*.ipv6_addresses)}"
 description = "the ipv6 ipv6_addresses for this timelord instance"
 }
