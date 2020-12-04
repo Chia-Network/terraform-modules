4b85f9a26c69ecead08d278a4acc5be290e62572
justinengland <justin@chia.net>
2020-12-03 21:48:31 -0700

    going back to 1 instance

--- timelord/outputs.tf
+++ timelord/outputs.tf
@@ -12,18 +12,3 @@ output "ipv6_addresses" {
 value       = aws_instance.timelord[0].ipv6_addresses
 description = "the ipv6 ipv6_addresses for this timelord instance"
 }
-
-output "public_dns2" {
-value       = aws_instance.timelord[1].public_dns
-description = "the public dns for this timelord instance"
-}
-
-output "public_ip2" {
-value       = aws_instance.timelord[1].public_ip
-description = "the public ipv4 for this timelord instance"
-}
-
-output "ipv6_addresses2" {
-value       = aws_instance.timelord[1].ipv6_addresses
-description = "the ipv6 ipv6_addresses for this timelord instance"
-}
--- timelord/variables.tf
+++ timelord/variables.tf
@@ -1,5 +1,5 @@
 variable "instance_count" {
-default = "2"
+default = "1"
 }
 
 variable "volume_size" {
