a7e94c8458bc4d9488e0c09c56f7fbd595038e16
justinengland <justin@jeng.land>
2020-12-14 14:57:19 -0700

    changing tag var set again

--- farmer/farmer.tf
+++ farmer/farmer.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "farmer" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [ var.main_sg,var.admin_sg ]
   key_name               = var.key_name
-  tags                   = "${merge(map("Name", "ChiaFarmer-${var.instance_name_tag}-${count.index + 1}", "application", "${var.application_tag}",), var.extra_tags)}"
+  tags                   = "${merge(map("Name", "ChiaFarmer-${var.instance_name_tag}-${count.index + 1}", "application", "${var.application_tag}", "branch", "${var.branch}",), var.extra_tags)}"
 
   provisioner "remote-exec" {
     inline = [
