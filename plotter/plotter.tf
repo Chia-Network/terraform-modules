15bb98a60224086b7450201f197fc429660f7525
justinengland <justin@jeng.land>
2021-01-21 00:25:35 -0700

    minor plotter module updates

--- plotter/plotter.tf
+++ plotter/plotter.tf
@@ -17,11 +17,21 @@ resource "aws_instance" "plotter" {
   application = var.application_tag
   }
 
-
+  provisioner "remote-exec" {
+    inline = [
+    "sudo mkfs.ext4 /dev/nvme1n1 && sudo mkdir /mnt/nvme && sudo mount /dev/nvme1n1 /mnt/nvme && sudo mkdir /mnt/nvme/plots && sudo mkdir /mnt/nvme/plots/temp && sudo mkdir /mnt/plots/final && sudo chown -R ubuntu:ubuntu /mnt/nvme",
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
 
   provisioner "file" {
     source      = "./chiapos"
-    destination = "/home/ubuntu/"
+    destination = "/mnt/nvme/"
     connection {
       type        = "ssh"
       host        = self.public_dns
@@ -32,7 +42,7 @@ resource "aws_instance" "plotter" {
 
   provisioner "file" {
     source      = "./chia-plotter/plot.sh"
-    destination = "/home/ubuntu/chiapos/plot.sh"
+    destination = "mnt/nvme/plots.sh"
     connection {
       type        = "ssh"
       host        = self.public_dns
@@ -43,8 +53,7 @@ resource "aws_instance" "plotter" {
 
   provisioner "remote-exec" {
     inline = [
-    "cd ./chiapos",
-    "sudo mkfs.ext4 /dev/nvme1n1 && sudo mkdir /mnt/nvme && sudo mount /dev/nvme1n1 /mnt/nvme && sudo mkdir /mnt/nvme/plots && sudo mkdir /mnt/nvme/plots/temp && sudo mkdir /mnt/plots/final && sudo chown -R ubuntu:ubuntu /mnt/nvme",
+    "cd /mnt/nvme/chiapos",
     "mkdir build && cd build && cmake ../ && cmake --build . -- -j 6",
     "sudo chmod a+x /home/ubuntu/chiapos/plot.sh",
     "sh /home/ubuntu/chiapos/plot.sh ${var.k_size}",
