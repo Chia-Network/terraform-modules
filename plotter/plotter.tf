7b6f5008cc50915cbff7e722214adcdf04e41ee2
justinengland <justin@jeng.land>
2021-01-22 14:56:57 -0700

    adding nohup to the plotter command

--- plotter/plotter.tf
+++ plotter/plotter.tf
@@ -56,7 +56,7 @@ resource "aws_instance" "plotter" {
     "cd /mnt/nvme/chiapos",
     "mkdir build && cd build && cmake ../ && cmake --build . -- -j 6",
     "sudo chmod a+x /mnt/nvme/plot.sh",
-    "sh /mnt/nvme/plot.sh ${var.k_size}",
+    "nohup sh /mnt/nvme/plot.sh ${var.k_size} &",
     "sleep 60",
     ]
     connection {
