57163c2aa6b8722d81558d8a3f7541cff435f7c2
justinengland <justin@jeng.land>
2021-03-04 16:30:55 -0700

    adding cert chain to iam certs

--- ssl/aws_iam_cert/aws_iam_cert.tf
+++ ssl/aws_iam_cert/aws_iam_cert.tf
@@ -7,6 +7,7 @@ resource "aws_iam_server_certificate" "certificate" {
   name_prefix       = var.ssl_domain
   certificate_body  = var.ssl_cert
   private_key       = var.ssl_key
+  certificate_chain = var.ssl_chain
 
   lifecycle {
     create_before_destroy = true
