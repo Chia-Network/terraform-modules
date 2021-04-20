cc65dd1d06565abc723f0f1e0cfc20e0fc673d2e
justinengland <justin@chia.net>
2021-04-19 23:26:05 -0600

    seedbox and arm node

---
+++ arm-node/arm-node.tf
@@ -0,0 +1,54 @@
+#
+# Ec2 Resources
+#  * arm node
+#
+
+
+resource "aws_instance" "arm-node" {
+  count                  = var.instance_count
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [ var.arm_sg,var.admin_sg ]
+  key_name               = var.key_name
+  tags                   = merge(map("Name", "ChiaArmNode${count.index + 1}-${var.instance_name_tag}", "application", "${var.application_tag}", "ref", "${var.ref}",), var.extra_tags)
+
+  provisioner "file" {
+    source      = "./report.sh"
+    destination = "/home/ubuntu/report.sh"
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+    "sudo chmod +x /home/ubuntu/report.sh && sh /home/ubuntu/report.sh ${self.public_dns} ${var.ref} ${var.instance_name_tag}",
+    "sleep 60",
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_dns
+      user        = var.ec2_user
+      private_key = file(var.ec2_key)
+    }
+  }
+
+  root_block_device {
+    volume_size = var.volume_size
+  }
+
+  lifecycle {
+  create_before_destroy = true
+
+  }
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "180m"
+  }
+}
