010db0dc9b43f5863b02230ff747b8de33067fbd
justinengland <justin@chia.net>
2021-04-20 18:09:58 -0600

    removing remote execs. bad keys

--- arm-node/arm-node.tf
+++ arm-node/arm-node.tf
@@ -9,33 +9,9 @@ resource "aws_instance" "arm-node" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.main_sg]
+  vpc_security_group_ids = [ var.main_sg ]
   key_name               = var.key_name
   tags                   = merge(map("Name", "ChiaArmNode${count.index + 1}-${var.instance_name_tag}", "application", "${var.application_tag}", "ref", "${var.ref}",), var.extra_tags)
-
-  provisioner "file" {
-    source      = "./report.sh"
-    destination = "/home/ubuntu/report.sh"
-    connection {
-      type        = "ssh"
-      host        = self.public_dns
-      user        = var.ec2_user
-      private_key = file(var.ec2_key)
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-    "sudo chmod +x /home/ubuntu/report.sh && sh /home/ubuntu/report.sh ${self.public_dns} ${var.ref} ${var.instance_name_tag}",
-    "sleep 60",
-    ]
-    connection {
-      type        = "ssh"
-      host        = self.public_dns
-      user        = var.ec2_user
-      private_key = file(var.ec2_key)
-    }
-  }
 
   root_block_device {
     volume_size = var.volume_size
