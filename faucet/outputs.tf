9fb20e856acc2c28d4a9c6617d6eaa69826919c9
Chris Marslender <chrismarslender@gmail.com>
2021-05-04 19:30:48 -0700

    add private ip to outputs

--- faucet/outputs.tf
+++ faucet/outputs.tf
@@ -12,3 +12,8 @@ output "ipv6_addresses" {
   value       = aws_instance.faucet-node[0].ipv6_addresses
   description = "the ipv6 ipv6_addresses for this instance"
 }
+
+output "private_ip" {
+  value       = aws_instance.faucet-node[0].private_ip
+  description = "the private ipv4 for this instance"
+}
