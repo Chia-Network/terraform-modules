adcdaa241892bfe686d2637c4c98b99e43276d31
Brandon Butler <b.butler@chia.net>
2023-08-02 15:17:28 -0700

    Allow overwrite of tags in ami_copy module

--- ami_copy/ami_copy.tf
+++ ami_copy/ami_copy.tf
@@ -5,11 +5,9 @@
 
 resource "aws_ami_copy" "ami_copy" {
   name              = var.image_copy_name
-  description       = "Copy an ami"
+  description       = "Copy of an ami. See the terraform-modules repo ami_copy module"
   source_ami_id     = var.source_ami_id
   source_ami_region = var.source_ami_region
 
-  tags = {
-    Name = var.image_copy_name
-  }
+  tags = var.tags
 }
